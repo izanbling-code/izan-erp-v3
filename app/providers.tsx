cda8d1141f6fd0ad332cd206e3d0bf6caea42f0a
Saad Qureshi <izanbling@gmail.com>
2026-09-25 12:11:25 +0500

    Fix build: Remove mounting bypass in Providers to allow static prerendering

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,18 +1,9 @@
 ﻿"use client";
 
 import { ThemeProvider } from "next-themes";
-import { useEffect, useState } from "react";
 import { SettingsProvider } from "./contexts/SettingsContext";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return <>{children}</>;
-
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <SettingsProvider>
